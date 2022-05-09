d06e6a960340604ecf11b9ee884a8ca10ff41281
dilshodbekisoqjonov <techprouz@gmail.com>
2022-05-09 08:12:13 -0500

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9c0c57c0-d638-4409-bd39-73ad655ed9c1';
+export const MODAL_CLOSE_SEC = 2.5;
